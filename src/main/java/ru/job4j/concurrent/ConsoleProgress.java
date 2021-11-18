f563f80b38fa6d1620290503f0f48099d383c640
MishailEx <Mishail.avram@mail.ru>
2021-11-18 16:47:47 +0300

    3. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,39 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                String symbol;
+                for (int i = 0; true; i++) {
+                    switch (i) {
+                        case (1):
+                            symbol = "|";
+                            break;
+                        case (2):
+                            symbol = "\\";
+                            break;
+                        default:
+                            symbol = "/";
+                            i = 0;
+                            break;
+                    }
+                    System.out.print("\r load: " + "Loading ..." + symbol);
+                    Thread.sleep(500);
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(10000);
+        progress.interrupt();
+    }
+
+
+}
