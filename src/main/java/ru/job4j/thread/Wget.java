2e377dc80bca462433a5736c83d4ade4ee281316
MishailEx <Mishail.avram@mail.ru>
2021-11-19 14:57:40 +0300

    4. Скачивание файла с ограничением. [#144271]

---
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -0,0 +1,42 @@
+package ru.job4j.thread;
+
+import java.io.*;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("note.txt")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                long start = System.currentTimeMillis();
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                long finish = System.currentTimeMillis();
+                long timeDownload = finish - start;
+                if (timeDownload < speed) {
+                    Thread.sleep(speed - timeDownload);
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
